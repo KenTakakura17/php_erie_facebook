513f48555e50e7fb5cb90ef33c007791715c0aa3
Your Name <eriegian457@gmail.com>
2026-02-19 08:48:38 +0800

    walang nausab

--- pages/createpage.php
+++ pages/createpage.php
@@ -9,25 +9,19 @@ include "resources/header.php"
     include "resources/navigation.php"
     ?>
 
-    <div class="crtprod">
+    <!-- <div class="crtprod">
         <h1 class="">Create User</h1>
         <form action="functions/createUser.php" method="POST">
-            <label for="" class="">Username</label>
-            <input type="text" name="user_name">
-            <label for="" class="">email</label>
-            <input type="text" name="email">
-            <label for="" class="">first_name</label>
-            <input type="text" name="first_name">
-            <label for="" class="">last_name</label>
-            <input type="text" name="last_name">
-            <label for="" class="">mobile_num</label>
-            <input type="text" name="mobile_num">
-            <label for="" class="">birthdate</label>
-            <input type="text" name="birthdate">
-            <label for="" class="">gender</label>
-            <input type="text" name="gender">
-            <label for="" class="">password</label>
-            <input type="text" name="password">
+            <label for="" class="">Name</label>
+            <input type="text" name="Name">
+            <label for="" class="">Age</label>
+            <input type="text" name="Age">
+            <label for="" class="">Address</label>
+            <input type="text" name="Address">
+            <label for="" class="">username</label>
+            <input type="text" name="username">
+            <label for="" class="">Password</label>
+            <input type="password" name="password">
             <button>Submit</button>
         </form>
     </div>
@@ -46,7 +40,7 @@ include "resources/header.php"
             <input type="date" name="prod_date_added">
             <button>Submit</button>
         </form>
-    </div>
+    </div> -->
 </body>
 
 </html>
